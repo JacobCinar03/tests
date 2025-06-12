4454ba01932669f35fde893ce54b55000f00c8ce
JacobCinar03 <jacobcinar03@gmail.com>
2025-06-13 00:31:17 +0300

    kadirr

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -21,7 +21,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	x1 = (unsigned char *)s1;
 	x2 = (unsigned char *)s2;
 	i = 0;
-	while (i < n && x1[i] && x2[i])
+	while (i < n && (s1[i] || s2[i]))
 	{
 		if (x1[i] != x2[i])
 			return (x1[i] - x2[i]);
