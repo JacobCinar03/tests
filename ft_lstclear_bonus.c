bbdbd2c7b599b6640ed130b8c097c4a767a05fd4
JacobCinar03 <jacobcinar03@gmail.com>
2025-06-14 20:04:11 +0300

    Add files via upload

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycinarog <ycinarog@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 11:40:47 by ycinarog          #+#    #+#             */
+/*   Updated: 2025/06/14 20:00:29 by ycinarog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*tmp;
+	int		i;
+
+	if (!lst || !del)
+		return ;
+	tmp = *lst;
+	i = 0;
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = tmp;
+		
+	}
+	*lst = NULL;
+}
