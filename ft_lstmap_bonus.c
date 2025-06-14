6c350942013a728ed4b6ee83db1105b462b0c9ca
JacobCinar03 <jacobcinar03@gmail.com>
2025-06-14 22:32:45 +0300

    final

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -9,7 +9,7 @@
 /*   Updated: 2025/06/14 20:00:45 by ycinarog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
+
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
@@ -34,37 +34,4 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		lst = lst->next;
 	}
 	return (new_lst);
-}
-*/
-
-#include "libft.h"
-
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-    t_list *new_list;
-    t_list *new_obj;
-    
-    if (!lst || !f || !del)
-        return (NULL);
-    new_list = NULL;
-    /* loop over the existing list */
-    while (lst)
-    {
-        /* create a new object with the content being the result
-         * of the application of the function f on the current element's
-         * content
-         */
-        new_obj = ft_lstnew(f(lst->content));
-        if (!new_obj)
-        {
-            /* if the new object is null, clear the new list */
-            ft_lstclear(&new_list, del);
-            return (NULL);
-        }
-        /* if there is a new object, add it to the back of the new list */
-        ft_lstadd_back(&new_list, new_obj);
-        lst = lst->next;
-    }
-    /* finally, we return the new list */
-    return (new_list);
 }
