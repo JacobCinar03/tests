3425f5c09457bb9ba9366387fdcb45e183fe953b
JacobCinar03 <jacobcinar03@gmail.com>
2025-06-15 11:03:26 +0300

    a

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_o;
@@ -35,28 +35,4 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (new_lst);
 }
-*/
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void*))
-{
-	t_list	*begin;
-	t_list	*work;
-
-	if (lst != NULL && f != NULL)
-	{
-		begin = ft_lstnew(f(lst->content));
-		work = begin;
-		while (lst->next)
-		{
-			lst = lst->next;
-			if ((work->next = ft_lstnew(ft_strdup(f(lst->content)))) == NULL)
-			{
-				ft_lstclear(&begin, del);
-				return (NULL);
-			}
-			work = work->next;
-		}
-		return (begin);
-	}
-	return (NULL);
-}
